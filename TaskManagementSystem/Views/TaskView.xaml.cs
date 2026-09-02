63f91b3a3e4d061a426309371ec672f843ce523c
AntonKenderov <anton.kenderov1@gmail.com>
2026-09-03 01:41:31 +0300

    feat(ui): Create All Tasks view

---
+++ TaskManagementSystem/Views/TaskView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TaskManagementSystem.Views
+{
+    /// <summary>
+    /// Interaction logic for TaskView.xaml
+    /// </summary>
+    public partial class TaskView : UserControl
+    {
+        public TaskView()
+        {
+            InitializeComponent();
+        }
+    }
+}
