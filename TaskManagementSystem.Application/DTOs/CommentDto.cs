1aa0e7ae967bce4d19c371a23a75abe7def83a56
AntonKenderov <anton.kenderov1@gmail.com>
2026-09-03 15:11:36 +0300

    feat: Create DTO for task details and comments

---
+++ TaskManagementSystem.Application/DTOs/CommentDto.cs
@@ -0,0 +1,16 @@
+﻿
+using TaskManagementSystem.Domain.Enums;
+
+namespace TaskManagementSystem.Application.DTOs
+{
+    public class CommentDto
+    {
+        public int Id { get; set; }
+        public required string Text { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public CommentType Type { get; set; }
+        public DateOnly? ReminderDate { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+        public string? ModifiedBy { get; set; }
+    }
+}
---
+++ TaskManagementSystem.Application/DTOs/TaskDetailDto.cs
@@ -0,0 +1,22 @@
+﻿
+using TaskManagementSystem.Domain.Enums;
+
+namespace TaskManagementSystem.Application.DTOs
+{
+    public class TaskDetailDto
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateOnly RequiredBy { get; set; }
+        public required string Description { get; set; }
+
+        public Status Status { get; set; }
+        public TaskType Type { get; set; }
+        public int AssignedToUserId { get; set; }
+        public UserDto? AssignedTo { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+        public string? ModifiedBy { get; set; }
+        public DateOnly? NextActionDate { get; set; }
+        public IReadOnlyList<CommentDto> Comments { get; set; } = new List<CommentDto>();
+    }
+}
