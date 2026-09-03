943b93ae70daca8f4984ff8c4424565360327017
AntonKenderov <anton.kenderov1@gmail.com>
2026-09-03 22:26:37 +0300

    feat: Add comment search filter DTO

---
+++ TaskManagementSystem.Application/DTOs/CommentSearchFilter.cs
@@ -0,0 +1,14 @@
+﻿using TaskManagementSystem.Domain.Enums;
+
+namespace TaskManagementSystem.Application.DTOs
+{
+    public class CommentSearchFilter
+    {
+        public string? Text { get; set; }
+        public CommentType? Type { get; set; }
+        public DateOnly? ReminderFrom { get; set; }
+        public DateOnly? ReminderTo { get; set; }
+        public DateTime? AddedFrom { get; set; }
+        public DateTime? AddedTo { get; set; }
+    }
+}
