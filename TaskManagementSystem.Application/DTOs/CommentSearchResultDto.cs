7c30d1d41e8a9b16aa6ad4fcde52c4ebb7ada16b
AntonKenderov <anton.kenderov1@gmail.com>
2026-09-03 22:25:00 +0300

    feat: Create DTO for comment searching

---
+++ TaskManagementSystem.Application/DTOs/CommentSearchResultDto.cs
@@ -0,0 +1,16 @@
+﻿
+using TaskManagementSystem.Domain.Enums;
+
+namespace TaskManagementSystem.Application.DTOs
+{
+    public class CommentSearchResultDto
+    {
+        public int Id { get; set; }
+        public int TaskItemId { get; set; }
+        public required string TaskDescription { get; set; }
+        public required string Text { get; set; }
+        public CommentType Type { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateOnly? ReminderDate { get; set; }     
+    }
+}
