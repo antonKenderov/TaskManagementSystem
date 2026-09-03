b485b9dee0a8130f1ca700a2a332afd903cd81e6
AntonKenderov <anton.kenderov1@gmail.com>
2026-09-03 21:02:52 +0300

    feat: Add DTO for new comments

---
+++ TaskManagementSystem.Application/DTOs/NewCommentDto.cs
@@ -0,0 +1,13 @@
+﻿
+using TaskManagementSystem.Domain.Enums;
+
+namespace TaskManagementSystem.Application.DTOs
+{
+    public class NewCommentDto
+    {
+        public int TaskItemId { get; set; }
+        public required string Text { get; set; }
+        public CommentType Type { get; set; }
+        public DateOnly? ReminderDate { get; set; }
+    }
+}
