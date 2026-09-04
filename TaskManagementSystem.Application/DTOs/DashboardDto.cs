35f47a5c7146fc9f27d25da48cb03b4aab970ea4
AntonKenderov <anton.kenderov1@gmail.com>
2026-09-04 08:56:09 +0300

    feat: Add DTO for dashboard and status count

---
+++ TaskManagementSystem.Application/DTOs/DashboardDto.cs
@@ -0,0 +1,12 @@
+﻿
+namespace TaskManagementSystem.Application.DTOs
+{
+    public class DashboardDto
+    {
+        public int TotalTasks { get; set; }
+        public int InProgressCount { get; set; }
+        public int CompletedCount { get; set; }
+        public int DueThisWeekCount { get; set; }
+        public IReadOnlyList<StatusCountDto> StatusBreakdown { get; set; } = new List<StatusCountDto>();
+    }
+}
---
+++ TaskManagementSystem.Application/DTOs/StatusCountDto.cs
@@ -0,0 +1,11 @@
+﻿
+using TaskManagementSystem.Domain.Enums;
+
+namespace TaskManagementSystem.Application.DTOs
+{
+    public class StatusCountDto
+    {
+        public Status Status { get; set; }
+        public int Count { get; set; }
+    }
+}
